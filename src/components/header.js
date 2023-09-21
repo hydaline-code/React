ad4a0bcdb5d8fdf1eee7cf0588884102744754e8
hydaline-code <djougancharlen@.com>
2023-09-21 11:26:02 +0200

    Make the website responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,34 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import logo from '../img/logo.png';
 
+
+
+
 function Header() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
   return (
     <header className="header">
       <div className="logo">
         <img className='logo' src={logo} alt="Logo" />
         <h4>Maths  Magicians</h4>
+        
+       
       </div>
-      <nav >
-        <ul className="nav-menu">
+    
+
+      <nav>
+
+        <ul className={isMenuOpen ? 'nav-menu open' : 'nav-menu'}>
           <li>
             <Link to="/">Home |</Link>
           </li>
@@ -21,7 +38,9 @@ function Header() {
           <li>
             <Link to="/quotes">Quotes |</Link>
           </li>
+                 
         </ul>
+        <FontAwesomeIcon icon={faBars} className="menu" onClick={handleMenuToggle}  />
       </nav>
     </header>
   );
