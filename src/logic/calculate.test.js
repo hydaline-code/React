ba71e21c086392feb538d43dbb4ab56ade348483
hydaline-code <djougancharlen@.com>
2023-09-22 04:38:43 +0200

    Testing for calculate and operate .js files

---
+++ src/logic/calculate.test.js
@@ -0,0 +1,25 @@
+import calculate from "./calculate.js";
+
+describe('calculate', () => {
+  it('should handle AC button', () => {
+    const result = calculate({ total: '10', next: '53', operation: '+' }, 'AC');
+    expect(result).toEqual({ total: null, next: null, operation: null });
+  });
+
+  it('should handle numeric button presses', () => {
+    const result = calculate({ total: '10', next: '53', operation: '+' }, '5');
+    expect(result).toEqual({ total: '10', next: '535', operation: '+' });
+  });
+
+  it('should handle decimal button', () => {
+    const result = calculate({ total: '10', next: '53', operation: '+' }, '.');
+    expect(result).toEqual({ total: '10', next: '53.', operation: '+' });
+  });
+
+  it('should handle equals button', () => {
+    const result = calculate({ total: '10', next: '53', operation: '+' }, '=');
+    expect(result).toEqual({ total: '63', next: null, operation: null });
+  });
+
+  
+});
